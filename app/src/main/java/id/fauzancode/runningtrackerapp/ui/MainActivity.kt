85bdcfd5b62315308aafbecb22b38e7eeefaed0c
mfauzan1202 <mfauzan120202@gmail.com>
2023-01-27 13:30:57 +0800

    Finish Slicing

---
+++ app/src/main/java/id/fauzancode/runningtrackerapp/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package id.fauzancode.runningtrackerapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.fauzancode.runningtrackerapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

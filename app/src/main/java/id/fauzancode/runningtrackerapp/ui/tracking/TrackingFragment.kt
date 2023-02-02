29b54430a7cc7f54b806314f07c790b12714d45a
mfauzan1202 <mfauzan120202@gmail.com>
2023-02-02 22:00:20 +0800

    done making map lifecycle handling

--- app/src/main/java/id/fauzancode/runningtrackerapp/ui/tracking/TrackingFragment.kt
+++ app/src/main/java/id/fauzancode/runningtrackerapp/ui/tracking/TrackingFragment.kt
@@ -1,24 +1,58 @@
 package id.fauzancode.runningtrackerapp.ui.tracking
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
 import id.fauzancode.runningtrackerapp.R
 import id.fauzancode.runningtrackerapp.databinding.FragmentTrackingBinding
+import id.fauzancode.runningtrackerapp.services.TrackingService
+import id.fauzancode.runningtrackerapp.services.TrackingService.Companion.ACTION_START
+import id.fauzancode.runningtrackerapp.services.TrackingService.Companion.ACTION_STOP
 
-class TrackingFragment : Fragment(R.layout.fragment_tracking) {
+class TrackingFragment : Fragment(R.layout.fragment_tracking), OnMapReadyCallback {
 
     private var _binding: FragmentTrackingBinding? = null
     private val binding get() = _binding!!
     private var map: GoogleMap? = null
+    private var isTracking = false
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentTrackingBinding.bind(view)
+        binding.apply {
+            mapView.onCreate(savedInstanceState)
+            mapView.getMapAsync(this@TrackingFragment)
+        }
+    }
 
-        binding.mapView.getMapAsync {
-            map = it
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+
+        binding.btnToggleRun.setOnClickListener {
+            toggleRun()
+        }
+    }
+
+    private fun toggleRun() {
+        isTracking = if (isTracking) {
+            binding.btnToggleRun.setImageResource(R.drawable.ic_start)
+            sendCommandToService(ACTION_STOP)
+            false
+        } else {
+            binding.btnToggleRun.setImageResource(R.drawable.ic_pause)
+            sendCommandToService(ACTION_START)
+            true
+        }
+    }
+
+    private fun sendCommandToService(actionPauseService: String) {
+        Intent(requireContext(), TrackingService::class.java).also {
+            it.action = actionPauseService
+            requireContext().startService(it)
         }
     }
 
--- app/src/main/java/id/fauzancode/runningtrackerapp/utils/Constants.kt
+++ app/src/main/java/id/fauzancode/runningtrackerapp/utils/Constants.kt
@@ -2,4 +2,5 @@ package id.fauzancode.runningtrackerapp.utils
 
 object Constants {
     const val RUNNING_DATABASE_NAME = "runs_db"
+    const val NOTIFICATION_ID = 1
 }

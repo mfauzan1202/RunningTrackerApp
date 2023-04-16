0d7333c7ede358146e11e5af9bcf3f5a659946a6
mfauzan1202 <mfauzan120202@gmail.com>
2023-04-17 04:19:52 +0800

    showing Total Runs Statistics

--- app/src/main/java/id/fauzancode/runningtrackerapp/ui/statistics/StatisticsFragment.kt
+++ app/src/main/java/id/fauzancode/runningtrackerapp/ui/statistics/StatisticsFragment.kt
@@ -2,6 +2,7 @@ package id.fauzancode.runningtrackerapp.ui.statistics
 
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -11,6 +12,7 @@ import id.fauzancode.runningtrackerapp.R
 import id.fauzancode.runningtrackerapp.adapters.RunAdapter
 import id.fauzancode.runningtrackerapp.databinding.FragmentStatisticsBinding
 import id.fauzancode.runningtrackerapp.ui.viewmodels.StatisticsViewModel
+import id.fauzancode.runningtrackerapp.utils.SortType
 
 @AndroidEntryPoint
 class StatisticsFragment : Fragment(R.layout.fragment_statistics) {
@@ -31,6 +33,43 @@ class StatisticsFragment : Fragment(R.layout.fragment_statistics) {
             btnBack.setOnClickListener {
                 findNavController().popBackStack()
             }
+
+            when(viewModel.sortType) {
+                SortType.DATE -> {
+                    spFilter.setSelection(0)
+                }
+                SortType.RUNNING_TIME -> {
+                    spFilter.setSelection(1)
+                }
+                SortType.DISTANCE -> {
+                    spFilter.setSelection(2)
+                }
+                SortType.CALORIES_BURNED -> {
+                    spFilter.setSelection(3)
+                }
+                SortType.AVG_SPEED -> {
+                    spFilter.setSelection(4)
+                }
+            }
+
+            spFilter.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    when(position) {
+                        0 -> viewModel.sortRuns(SortType.DATE)
+                        1 -> viewModel.sortRuns(SortType.RUNNING_TIME)
+                        2 -> viewModel.sortRuns(SortType.DISTANCE)
+                        3 -> viewModel.sortRuns(SortType.CALORIES_BURNED)
+                        4 -> viewModel.sortRuns(SortType.AVG_SPEED)
+                    }
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {}
+            }
         }
     }
 
@@ -43,7 +82,7 @@ class StatisticsFragment : Fragment(R.layout.fragment_statistics) {
     }
 
     private fun subscribeToObservers() {
-        viewModel.runSortedByDate.observe(viewLifecycleOwner) {
+        viewModel.runs.observe(viewLifecycleOwner) {
             it?.let {
                 runAdapter.submitList(it)
             }
---
+++ app/src/main/java/id/fauzancode/runningtrackerapp/utils/SortType.kt
@@ -0,0 +1,5 @@
+package id.fauzancode.runningtrackerapp.utils
+
+enum class SortType {
+    DATE, RUNNING_TIME, AVG_SPEED, DISTANCE, CALORIES_BURNED
+}
